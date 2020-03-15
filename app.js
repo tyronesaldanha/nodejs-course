5b299b373acbfc54199074b4e7f69934b38e07ea
Tyrone <tyronesaldanha@gmail.com>
2020-03-15 22:49:05 +0530

    added 2 middleware layers

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
-const http=require('http');
-//const routes=require('./routes');//exported file route.js
-const express=require('express');
-const app=express();
-const server=http.createServer();
+const http = require('http');
+
+const express = require('express');
+const app = express();
+app.use((req, res, next) => {
+    console.log('1st middleware');
+    next();//goes to next app.use()
+});
+
+app.use((req, res, next) => {
+    console.log('2nd middleware');
+    res.send('<h1>hello</h1>');
+});
+
+const server = http.createServer(app);
 
 server.listen(3000);
