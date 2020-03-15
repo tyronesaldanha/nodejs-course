35ccfb7285fa0ff36dbf54cc02501c4890ce03b9
Tyrone <tyronesaldanha@gmail.com>
2020-03-15 22:08:42 +0530

    express js installed, removed route.js file and references

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const http=require('http');
-const routes=require('./routes');//exported file route.js
-
-const server=http.createServer(routes);
+//const routes=require('./routes');//exported file route.js
+const express=require('express');
+const app=express();
+const server=http.createServer();
 
 server.listen(3000);
